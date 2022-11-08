b96daf9f188a565f0677f6d3e43d3bf99cead726
asiq rahman <aasiqrrahman@yahoo.com>
2022-11-08 23:02:33 +0600

    Add project files.

---
+++ Sword Damage Game/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sword_Damage_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        SwordDamage swordDamage = new SwordDamage();
+        public MainWindow()
+        {
+            InitializeComponent();
+            swordDamage.SetMagic(false);
+            swordDamage.SetFlaming(false);
+            RollDice();
+        }
+
+        public void RollDice()
+        {
+            swordDamage.Roll = random.Next(1, 7) + random.Next(1, 7) + random.Next(1, 7);
+            swordDamage.SetFlaming(flamming.IsChecked.Value);
+            swordDamage.SetMagic(magic.IsChecked.Value);
+            
+            DisplayDamage();
+        }
+
+        void DisplayDamage()
+        {
+            damage.Text = "Rolled " + swordDamage.Roll + " for " + swordDamage.Damage + " HP";
+        }
+
+        private void magic_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetMagic(true);
+            DisplayDamage();
+        }
+
+        private void Magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetMagic(false);
+            DisplayDamage();
+        }
+
+        private void Flamming_Checked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(true);
+            DisplayDamage();
+        }
+        private void Flamming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            swordDamage.SetFlaming(false);
+            DisplayDamage();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+    }
+}
---
+++ Sword Damage Game/SwordDamage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sword_Damage_Game
+{
+    class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+        public int Roll;
+        public decimal MagiMultiplier = 1M;
+        public int FlamingDamage = 0;
+        public int Damage;
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagiMultiplier) + BASE_DAMAGE + FLAME_DAMAGE;
+        }
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagiMultiplier = 1.75M;
+            }
+            else
+            {
+                MagiMultiplier = 1M;
+            }
+            CalculateDamage();
+        }
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if (isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+    }
+}
